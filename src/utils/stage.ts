26b93dccc024f62ae495d99a841d1b4cd3f8fda2
imad-npm <couty@example.com>
2025-10-05 20:49:37 +0100

    refct

--- src/utils/stage.ts
+++
@@ -1,7 +0,0 @@
-import { TenderStage } from '../types/tender';
-import { stageConfig } from './stageConfig';
-
-export const getActionDescription = (nextStage: TenderStage) => {
-  const nextStageConfig = stageConfig[nextStage];
-  return `This will move the tender to "${nextStageConfig.label}" stage.`;
-};
